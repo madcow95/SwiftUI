52d5997000cfeaecc27725e745835eed76bd5be6
madcow95 <lobasketve@naver.com>
2023-07-18 21:58:21 +0900

    Feat : 신규 프로젝트 생성
    SwiftUI tutorial 프로젝트 생성

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by ChoiKwangWoo on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
